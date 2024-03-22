59ba6de8444c9a427175d22a3220b16e07440035
LucasQuinhones <lucasquinhones10@gmail.com>
2024-03-22 10:41:24 -0300

    Second Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  void _getListApi(){
+    http.get(
+      Uri.https('api.themoviedb.org', '/4/list/1'),
+      headers: {
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NGJkZWVmNDg5ODhlMTk0MWVlN2JmNWY0MTgzMWQ0NyIsInN1YiI6IjY1ZmM0NGM2MGJjNTI5MDE0OWFlNDY5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CVkb6GF2r97G91PBwGuFC9vScg8S-6ZGAjyxPyoKMn4',
+        'content-type': 'application/json'
+      }
+    ).then(
+      (Response value) => print(value.body),
+    );
+  }
+
+
+  @override
+  void initState(){
+    super.initState();
+    _getListApi();
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-    http
-      .get(
-        Uri.https('gianflutter.free.beeceptor.com', '/test')
-      )
-      .then(
-        (value) => print(value.body),
-      );
-
     return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
